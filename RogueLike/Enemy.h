50284fd674db197fa897e0c02d8b64d07f76801a
Nate Lincoln <nate.school42@gmail.com>
2016-02-13 01:34:13 -0600

    Header file was missing from previous commit

--- RogueLike/Enemy.h
+++ RogueLike/Enemy.h
@@ -5,6 +5,7 @@
 #include "CollisionSystem.h"
 #include "Component.h"
 #include "World.h"
+#include "EntityCommon.h"
 
 enum class EventType : uint;
 class RenderManager;
@@ -17,22 +18,29 @@ struct EnemyDescription : EntityDescription
 	uint BaseExperience;
 };
 
+using AIFunction = std::function<void()>;
+
 class Enemy : public Entity
 {
+	enum class StateType { Rest, Pursuit, LastKnown };
+
 	std::vector<Item> m_Inventory;
 	CollisionCallback m_CollisionCallback;
+
+	StateType m_CurrentState;
+
 
 	EnemyManager* m_EnemyManagerRef;
 	EventManager* m_EventManagerRef;
 	WorldGenerator* m_WorldRef;
 
 	WorldPos m_PrevTarget;
-	bool m_PrevTargetIsSet;
-
 
 	int m_MaxHP;
 	int m_Health;
 	int m_DeathHP;
+
+	std::map<StateType, AIFunction> InitializeAI();
 public:
 	std::function<void(EventType)> MovementCallback();
 
